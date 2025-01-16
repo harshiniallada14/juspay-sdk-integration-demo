2abbb6ea56a2fc4254a1f6553a7d1e00b3f13953
harshiniallada14 <harshini.allada@juspay.in>
2025-01-16 12:24:00 +0530

    Create session-api-response.sh

---
+++ payment_links/session-api-response.sh
@@ -0,0 +1,47 @@
+{
+    "status": "NEW",
+    "id": "ordeh_662673ccb6ac41e153afc4",
+    "order_id": "order_dummy_1",
+    "payment_links": {
+        "web": "https://sandbox.hyperpg.in/orders/ordeh_662673ccb6ac41e153afc4/payment-page",
+        "expiry": "2023-12-30T21:46:57Z"
+    },
+    "sdk_payload": {
+        "requestId": "af2f666419a24155991764369ccaed86",
+        "service": "in.hyperpg",
+        "payload": {
+      "amount": "100.0",
+      "currency": "INR",
+      "orderId": "order_a_b_id_dummy_1",
+      "merchantId": "global_sandbox",
+      "clientId": "test",
+            "customerId": "customer_id",
+            "customerPhone": "8888899999",
+            "customerEmail": "abcd@gmail.com",
+      "sendMail": true,
+      "sendSms": true,
+      "send_whatsapp": false,
+      "clientAuthToken": "tkn_9e414692313a47cd",
+            "clientAuthTokenExpiry": "2023-12-28T13:24:57Z",
+            "environment": "sandbox",
+      "gatewayId": "12",
+      "metadata.expiryinmins": "3397",
+            "action": "paymentPage",
+             "udf1": "udf1-dummy",
+      "udf2": "udf2-dummy",
+      "udf3": "udf3-dummy",
+      "udf4": "udf4-dummy",
+      "udf5": "udf5-dummy",
+      "udf6": "udf6-dummy"
+      "udf7": "udf7-dummy",
+      "udf8": "udf8-dummy",
+      "udf9": "udf9-dummy",
+      "udf10": "udf10-dummy",
+      "source_object": "PAYMENT_LINK",
+            "returnUrl": "https://hyperpg.in/",
+            "description": "This is Payment Description."
+        },
+        "expiry": "2023-12-30T21:46:57Z"
+    },
+    "order_expiry": "2023-12-30T21:46:57Z"
+}
