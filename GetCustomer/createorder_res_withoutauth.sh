4d133b39446143d94a196dc61bef668dbddbe8e8
harshiniallada14 <harshini.allada@juspay.in>
2025-01-29 16:20:18 +0530

    Create createorder_res_withoutauth.sh

---
+++ GetCustomer/createorder_res_withoutauth.sh
@@ -0,0 +1,11 @@
+{
+  "status": "CREATED",
+  "status_id": 1,
+  "id": "ord_e294a26e66ad4336a992ceab81ad704c",
+  "order_id": "14183944763",
+  "payment_links": {
+    "web": "https://api.hyperpg.in/merchant/pay/ord_e294a26e66ad4336a992ceab81ad704c",
+    "mobile": "https://api.hyperpg.in/merchant/pay/ord_e294a26e66ad4336a992ceab81ad704c?mobile=true",
+    "iframe": "https://api.hyperpg.in/merchant/ipay/ord_e294a26e66ad4336a992ceab81ad704c"
+  }
+}
