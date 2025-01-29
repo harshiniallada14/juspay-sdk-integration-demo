c25fb37039abc601ed9811fcd6261a8e73d24fcf
harshiniallada14 <harshini.allada@juspay.in>
2025-01-29 16:12:01 +0530

    Create tpv_payment_methods.sh

---
+++ GetCustomer/tpv_payment_methods.sh
@@ -0,0 +1,274 @@
+{
+"tpv_payment_methods": [
+        {
+            "description": "Allahabad Bank",
+            "hyperpg_bank_code": "JP_ALLB",
+            "payment_method": "JP_ALLB",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "Andhra Bank",
+            "hyperpg_bank_code": "JP_ANDHRA",
+            "payment_method": "JP_ANDHRA",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "AU Small Finance Bank",
+            "hyperpg_bank_code": "JP_AUB",
+            "payment_method": "JP_AUB",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "Axis Bank",
+            "hyperpg_bank_code": "JP_AXIS",
+            "payment_method": "JP_AXIS",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "Bandhan Bank",
+            "hyperpg_bank_code": "JP_BANDHAN",
+            "payment_method": "JP_BANDHAN",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "BOB Card",
+            "hyperpg_bank_code": "JP_BOB",
+            "payment_method": "JP_BOB",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "Bank of India",
+            "hyperpg_bank_code": "JP_BOI",
+            "payment_method": "JP_BOI",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "Bank of Maharashtra",
+            "hyperpg_bank_code": "JP_BOM",
+            "payment_method": "JP_BOM",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "The Catholic Syrian Bank",
+            "hyperpg_bank_code": "JP_CSB",
+            "payment_method": "JP_CSB",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "Canara Bank",
+            "hyperpg_bank_code": "JP_CANR",
+            "payment_method": "JP_CANR",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "Central Bank of India",
+            "hyperpg_bank_code": "JP_CBI",
+            "payment_method": "JP_CBI",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "City Union Bank",
+            "hyperpg_bank_code": "JP_CUB",
+            "payment_method": "JP_CUB",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "Corporation Bank",
+            "hyperpg_bank_code": "JP_CORP",
+            "payment_method": "JP_CORP",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "Deutsche Bank",
+            "hyperpg_bank_code": "JP_DEUT",
+            "payment_method": "JP_DEUT",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "DCB",
+            "hyperpg_bank_code": "JP_DCB",
+            "payment_method": "JP_DCB",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "Dhanalakshmi Bank",
+            "hyperpg_bank_code": "JP_DLS",
+            "payment_method": "JP_DLS",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "Equitas Bank",
+            "hyperpg_bank_code": "JP_EQUITAS",
+            "payment_method": "JP_EQUITAS",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "Federal Bank",
+            "hyperpg_bank_code": "JP_FED",
+            "payment_method": "JP_FED",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "HDFC Bank",
+            "hyperpg_bank_code": "JP_HDFC",
+            "payment_method": "JP_HDFC",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "ICICI Bank",
+            "hyperpg_bank_code": "JP_ICICI",
+            "payment_method": "JP_ICICI",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "IDBI Bank",
+            "hyperpg_bank_code": "JP_IDBI",
+            "payment_method": "JP_IDBI",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "IDFC FIRST Bank",
+            "hyperpg_bank_code": "JP_IDFC",
+            "payment_method": "JP_IDFC",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "Indian Bank",
+            "hyperpg_bank_code": "JP_INDB",
+            "payment_method": "JP_INDB",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "Indian Overseas Bank",
+            "hyperpg_bank_code": "JP_IOB",
+            "payment_method": "JP_IOB",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "IndusInd Bank",
+            "hyperpg_bank_code": "JP_INDUS",
+            "payment_method": "JP_INDUS",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "Jammu and Kashmir Bank",
+            "hyperpg_bank_code": "JP_JNK",
+            "payment_method": "JP_JNK",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "Karnataka Bank",
+            "hyperpg_bank_code": "JP_KARN",
+            "payment_method": "JP_KARN",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "Karur Vysya Bank",
+            "hyperpg_bank_code": "JP_KVB",
+            "payment_method": "JP_KVB",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "Kotak Mahindra Bank",
+            "hyperpg_bank_code": "JP_KOTAK",
+            "payment_method": "JP_KOTAK",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "Punjab National Bank",
+            "hyperpg_bank_code": "JP_PNB",
+            "payment_method": "JP_PNB",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "Saraswat Bank",
+            "hyperpg_bank_code": "JP_SARASB",
+            "payment_method": "JP_SARASB",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "South Indian Bank",
+            "hyperpg_bank_code": "JP_SOIB",
+            "payment_method": "JP_SOIB",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "Standard Chartered Bank",
+            "hyperpg_bank_code": "JP_SCB",
+            "payment_method": "JP_SCB",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "SBI",
+            "hyperpg_bank_code": "JP_SBI",
+            "payment_method": "JP_SBI",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "The Shamrao Vithal Cooperative Bank",
+            "hyperpg_bank_code": "JP_SVCB",
+            "payment_method": "JP_SVCB",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "Tamilnad Mercantile Bank",
+            "hyperpg_bank_code": "JP_TMB",
+            "payment_method": "JP_TMB",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "Lakshmi Vilas Bank",
+            "hyperpg_bank_code": "JP_LVB",
+            "payment_method": "JP_LVB",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "RBL Bank",
+            "hyperpg_bank_code": "JP_RBL",
+            "payment_method": "JP_RBL",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "Union Bank of India",
+            "hyperpg_bank_code": "JP_UBI",
+            "payment_method": "JP_UBI",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "UCO Bank",
+            "hyperpg_bank_code": "JP_UCOB",
+            "payment_method": "JP_UCOB",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "Yes Bank",
+            "hyperpg_bank_code": "JP_YESB",
+            "payment_method": "JP_YESB",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "Jammu and Kashmir",
+            "hyperpg_bank_code": "JP_JMK",
+            "payment_method": "JP_JMK",
+            "payment_method_type": "NB"
+        },
+        {
+            "description": "UPI Intent Pay",
+            "hyperpg_bank_code": "PAY",
+            "payment_method": "PAY",
+            "payment_method_type": "UPI"
+        },
+        {
+            "description": "UPI Collect",
+            "hyperpg_bank_code": "COLLECT",
+            "payment_method": "COLLECT",
+            "payment_method_type": "UPI"
+        },
+        {
+            "description": "DBS Bank",
+            "hyperpg_bank_code": "JP_DBS",
+            "payment_method": "JP_DBS",
+            "payment_method_type": "NB"
+        }
+    ]
+}
