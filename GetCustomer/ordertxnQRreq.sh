24881a4533b691b953f3f757d7e080e35fac24f4
harshiniallada14 <harshini.allada@juspay.in>
2025-01-29 16:34:33 +0530

    Update ordertxnQRreq.sh

--- GetCustomer/ordertxnQRreq.sh
+++ GetCustomer/ordertxnQRreq.sh
@@ -4,13 +4,13 @@ curl --location --request POST 'https://api.hyperpg.in/orders' \
 --form 'amount="1"' \
 --form 'currency="INR"' \
 --form 'customer_id="cth_w2gbGjGi8BUkLgCo"' \
---form 'customer_email="krishna.gprasad@juspay.in"' \
---form 'customer_phone="7093125824"' \
+--form 'customer_email="abc@gmail.com"' \
+--form 'customer_phone="9999988888"' \
 --form 'description="NA"' \
 --form 'product_id="5"' \
 --form 'return_url="http://www.google.com/"' \
 --form 'options.get_client_auth_token="true"' \
---form 'metadata.JUSPAY:gateway_reference_id="testmode"' \
+--form 'metadata.HYPERPG:gateway_reference_id="testmode"' \
 –-form 'udf10="RiderQR"' \
 –-form 'order_fullfillment_threshold_in_mins="60"' \
 --form 'options.get_upi_qr="true"'
